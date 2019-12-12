937063cd75f0e1d28aaf47c78c98f401cd2a3174
Brittany <britloy91@gmail.com>
2019-12-12 18:19:07 -0500

    Re-initializing repo

---
+++ javascript.js
@@ -0,0 +1,10 @@
+function AboutMe() {
+var aboutElement = document.getElementById("para");
+aboutElement.scrollIntoView();
+}
+
+function ContactMe() {
+    var aboutElement = document.getElementById("Info");
+    aboutElement.scrollIntoView();
+    }
+    
