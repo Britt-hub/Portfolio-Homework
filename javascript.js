d34d05beab1b5bda259f33c1936acfb842ac60aa
Brittany <britloy91@gmail.com>
2019-12-13 14:56:56 -0500

    front style changed

--- javascript.js
+++ javascript.js
@@ -1,10 +1,18 @@
 function AboutMe() {
 var aboutElement = document.getElementById("para");
 aboutElement.scrollIntoView();
+window.scrollBy(0, -64);
 }
 
 function ContactMe() {
     var aboutElement = document.getElementById("Info");
     aboutElement.scrollIntoView();
+    window.scrollBy(0, -64);
     }
-    
+
+    function Portfolio() {
+        var aboutElement = document.getElementById("Portfolio");
+        aboutElement.scrollIntoView();
+        window.scrollBy(0, -64);
+        }
+        
