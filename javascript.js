50c02057c3f06446a6f7dfe0fa78d9199ef5a058
Brittany <britloy91@gmail.com>
2020-01-02 18:28:20 -0500

    size change,css, and imagine

--- javascript.js
+++ javascript.js
@@ -1,24 +1,30 @@
 function AboutMe() {
-var aboutElement = document.getElementById("para");
-aboutElement.scrollIntoView();
-window.scrollBy(0, -64);
+    var aboutElement = document.getElementById("para");
+    aboutElement.scrollIntoView();
+    window.scrollBy(0, -64);
+    aboutElement.className += "animated slideInUp ";
 }
 
 function ContactMe() {
     var aboutElement = document.getElementById("Info");
     aboutElement.scrollIntoView();
     window.scrollBy(0, -64);
-    }
+    aboutElement.className += "animated slideInUp ";
+
+}
 
 function Portfolio() {
     var aboutElement = document.getElementById("Portfolio");
     aboutElement.scrollIntoView();
     window.scrollBy(0, -64);
-        }
-        
- function Home() {
+    aboutElement.className += "animated slideInUp ";
+
+}
+
+function Home() {
     var aboutElement = document.getElementById("Home");
     aboutElement.scrollIntoView();
     window.scrollBy(0, -64);
-            }
-           
+    aboutElement.className += "animated slideInUp ";
+
+}
